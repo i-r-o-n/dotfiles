f1ce8e8043f446c5334f1421451ef0d6554b9654
dre <graphics.iron@gmail.com>
2024-03-26 10:33:45 -0700

    updated lualine and theme

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,59 @@
+-- return {
+--   -- { "marko-cerovac/material.nvim" },
+--   {
+--     "projekt0n/github-nvim-theme",
+--     lazy = false,
+--   },
+--   {
+--     "LazyVim/LazyVim",
+--     opts = {
+--       colorscheme = "github_dark_default",
+--     },
+--   },
+-- }
 return {
-  -- { "marko-cerovac/material.nvim" },
-  { "projekt0n/github-nvim-theme" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "github_dark_default",
+      colorscheme = "catppuccin",
+    },
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      term_colors = true,
+      transparent_background = true,
+      -- styles = {
+      --   comments = {},
+      --   conditionals = {},
+      --   loops = {},
+      --   functions = {},
+      --   keywords = {},
+      --   strings = {},
+      --   variables = {},
+      --   numbers = {},
+      --   booleans = {},
+      --   properties = {},
+      --   types = {},
+      -- },
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      integrations = {
+        telescope = {
+          enabled = true,
+          -- style = "nvchad",
+        },
+        dropbar = {
+          enabled = true,
+          color_mode = true,
+        },
+      },
     },
   },
 }
