496db646bf712fae322ce167b12ac0cbea80231a
dre <alipson@pm.me>
2024-03-26 23:01:05 -0700

    updated latex configs

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -7,6 +7,17 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- Key mapping for correcting the last spelling mistake in insert mode
 vim.api.nvim_set_keymap("i", "<C-l>", "<c-g>u<Esc>[s1z=`]a<c-g>u", { noremap = true })
 
+-- ultisnips
+-- vim.g.UltiSnipsExpandTrigger = "<tab>"
+-- vim.g.UltiSnipsJumpForwardTrigger = "<tab>"
+-- vim.g.UltiSnipsJumpBackwardTrigger = "<s-tab>"
+
+vim.cmd([[
+let g:UltiSnipsExpandTrigger="<tab>"
+let g:UltiSnipsJumpForwardTrigger="<tab>"
+let g:UltiSnipsJumpBackwardTrigger="<s-tab>"
+]])
+
 -- nvim tmux navigator
 -- require("nvim-tmux-navigation").setup({
 --   disable_when_zoomed = true, -- defaults to false
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,8 +7,14 @@ vim.env.PATH = vim.env.PATH .. ":/usr/local/texlive/2024/bin/x86_64-linux"
 vim.o.shell = "/usr/bin/zsh"
 vim.o.runtimepath = vim.o.runtimepath .. ",~/.config/nvim/lua/"
 
--- latex
 vim.o.conceallevel = 1
+-- latex
+vim.g.tex_flavor = "latex"
+vim.g.vimtex_view_method = "zathura"
+vim.g.vimtex_quickfix_mode = 0
+
+-- ultisnips
+vim.g.UltiSnipsSnippetDirectories = { "snips" }
 
 vim.o.spelllang = "en_us"
 -- vim.o.spell = true
