4b6443a4d6205ba179b66eef3c6f5a2c8d6965be
dre <alipson@pm.me>
2024-03-29 14:43:28 -0700

    update p10k theme

---
+++ zsh/aliases.zsh
@@ -0,0 +1,11 @@
+
+alias cd="z"
+
+alias ssh="ssh -F ~/.config/ssh/config"
+
+
+# clipboard
+alias "copy=xclip" # copy to X clipboard (register *)
+alias "copys=xclip -selection clipboard" # copy to system wide clipboard (register +)
+alias "put=xclip -o" # output copied content (paste)
+alias "puts=xclip -o -selection clipboard" # paste from system wide clipboard (equivalent to `v -selection clipboard`)
