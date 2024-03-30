efcc9af16bbb494fc92c5acf5ee003ab9dfd5dae
dre <alipson@pm.me>
2024-03-29 22:11:35 -0700

    working on system setup script

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -2,13 +2,22 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
--- Autocommand to enable spell checking for certain file types
+-- autocommand to enable spell checking for certain file types
 vim.cmd([[
   autocmd FileType markdown,tex,html,text setlocal spell
 ]])
 
 -- colorizer
 require("colorizer").setup()
+
+-- disable comment formatting on lines opened after comment
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "*",
+  callback = function()
+    vim.opt.formatoptions:remove("r")
+    vim.opt.formatoptions:remove("o")
+  end,
+})
 
 -- keyboard layout force change
 -- vim.api.nvim_create_autocmd("InsertEnter", {
--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
 #!/bin/sh
+
+# yay-bin
+git clone https://aur.archlinux.org/yay-bin.git
+cd yay-bin
+MAKEFLAGS="--noconfirm" makepkg -si --noconfirm
+
+# pac-pacman-aliases
+yay -S pac-pacman-aliases --noconfirm
