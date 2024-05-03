27ddef39fdeaca17223d01f3c06bd4fdd989c482
dre <alipson@pm.me>
2024-05-02 23:11:55 -0700

    fixed add input group rules

--- kanata/input-group.sh
+++ kanata/input-group.sh
@@ -5,11 +5,12 @@ sudo groupadd uinput
 sudo usermod -aG input $USER
 sudo usermod -aG uinput $USER
 
-sudo touch /etc/udev/rules.d/00-input.rules
+sudo su - $USER
 
-echo 'KERNEL==uinput, MODE=0660, GROUP=uinput, OPTIONS+=static_node=uinput' | sudo tee /etc/udev/rules.d/01-input.rules
+echo 'KERNEL=="uinput", MODE="0660", GROUP="uinput", OPTIONS+="static_node=uinput"' | sudo tee /etc/udev/rules.d/01-input.rules
 
-sudo udevadm control --reload-rules && sudo udevadm trigger
+sudo udevadm control --reload-rules
+sudo udevadm trigger
 
 # verify
 ls -l /dev/uinput
