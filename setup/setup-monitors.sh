81a5bb9e068bcdf4ecbef509656b2a2abdaa0466
dre <alipson@pm.me>
2024-06-15 22:15:20 -0700

    remove test in setup-monitors.sh

--- setup/setup-monitors.sh
+++ setup/setup-monitors.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
 
-# test
-# export DISPLAY=:0
-
 # make sure to spoof your monitor resolution in your browser anti-fingerprinting software
 # xrandr --output DP-2 --primary --mode 3840x2160 --rate 60 --pos 1080x0 --rotate normal --output DP-4 --mode 1920x1080 --rate 280 --pos 0x0 --rotate left
