b928384db63be7ce4b678f69bc0f454a68a72bb4
dre <alipson@pm.me>
2024-06-15 22:07:32 -0700

    removed monitor in setup and also in /etc/X11/xorg.conf.d

--- setup/setup-monitors.sh
+++ setup/setup-monitors.sh
@@ -4,4 +4,4 @@
 # export DISPLAY=:0
 
 # make sure to spoof your monitor resolution in your browser anti-fingerprinting software
-xrandr --output DP-2 --primary --mode 3840x2160 --rate 60 --pos 1080x0 --rotate normal --output DP-4 --mode 1920x1080 --rate 280 --pos 0x0 --rotate left
+# xrandr --output DP-2 --primary --mode 3840x2160 --rate 60 --pos 1080x0 --rotate normal --output DP-4 --mode 1920x1080 --rate 280 --pos 0x0 --rotate left
