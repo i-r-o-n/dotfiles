b7a62a105f426f1113ffc75c79dcdb33e4304147
dre <alipson@pm.me>
2024-07-09 14:52:52 -0700

    update zen mode plugin to lazy load on command

--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -1,6 +1,7 @@
 return {
   {
     "folke/zen-mode.nvim",
+    cmd = "ZenMode",
     keys = {
       { "<leader>uz", "<cmd>ZenMode<CR>", desc = "Toggle Zen Mode", mode = { "n", "x", "o" } },
     },
