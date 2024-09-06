c1d7e37b3c7f9c8720c09a8b643313c912c5292d
dre <alipson@pm.me>
2024-09-06 13:16:54 -0700

    gave up on bold+italic font

---
+++ alacritty/font_test.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "Normal"
+echo -e "\e[1mBold\e[0m"
+echo -e "\e[3mItalic\e[0m"
+echo -e "\e[1m\e[3mBold Italic\e[0m"
+echo -e "\e[4mUnderline\e[0m"
+echo -e "\e[1m\e[4mBold Underline\e[0m"
+echo -e "\e[3m\e[4mItalic Underline\e[0m"
+echo -e "\e[1m\e[3m\e[4mBold Italic Underline\e[0m"
