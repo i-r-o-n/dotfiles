dbce762c6de8faed6a893cd30e88805c76ed8a05
dre <alipson@pm.me>
2024-09-19 01:44:59 -0700

    removed xkbswitch plugin temporarily bc was tired of warning msg + i wasn't using it

--- nvim/lua/plugins/xkbswitch.lua
+++ nvim/lua/plugins/xkbswitch.lua
@@ -1,8 +1,8 @@
 return {
-  {
-    "ivanesmantovich/xkbswitch.nvim",
-    config = function()
-      require("xkbswitch").setup({})
-    end,
-  },
+  -- {
+  --   "ivanesmantovich/xkbswitch.nvim",
+  --   config = function()
+  --     require("xkbswitch").setup({})
+  --   end,
+  -- },
 }
