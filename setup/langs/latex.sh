4249635dc1bcf5ffcb40a870441c6fae809d6327
dre <a@alipson.me>
2024-09-29 22:11:17 -0700

    modified setup latex lang script (it needs further work)

--- setup/langs/latex.sh
+++ setup/langs/latex.sh
@@ -3,7 +3,7 @@
 # TexLive
 curl -L -o install-tl-unx.tar.gz https://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
 zcat <install-tl-unx.tar.gz | tar xf -
-cd install-tl-* || return
+cd install-tl-* || die
 sudo perl ./install-tl --no-interaction
 
 # this takes some time to complete
