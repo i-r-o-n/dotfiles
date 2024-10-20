a666b89d8f476f27d19b8d8947fa5452fdff0f90
dre <a@alipson.me>
2024-10-19 23:04:01 -0700

    update zen mode lua plugin option to enable kitty connection

--- nvim/lua/plugins/zen-mode.lua
+++ nvim/lua/plugins/zen-mode.lua
@@ -5,6 +5,17 @@ return {
     keys = {
       { "<leader>uz", "<cmd>ZenMode<CR>", desc = "Toggle Zen Mode", mode = { "n", "x", "o" } },
     },
+    opts = {
+      window = {
+        width = 100,
+      },
+      plugins = {
+        kitty = {
+          enabled = true,
+          font = "+8",
+        },
+      },
+    },
   },
   { "folke/twilight.nvim" },
 }
