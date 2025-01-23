40860750254cbfbdafe01ce4107aacc8bae8acce
dre <a@alipson.me>
2025-01-22 17:01:22 -0800

    update tex snippets

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -45,3 +45,6 @@ require("lazy").setup({
     },
   },
 })
+
+-- scroll off at end of file on insert mode
+require("scrollEOF").setup({ insert_mode = true })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -46,6 +46,3 @@ vim.g.vimtex_compiler_latexmk = {
 
 -- ultisnips
 vim.g.UltiSnipsSnippetDirectories = { "~/.config/nvim/snips" }
-
--- scroll off at end of file on insert mode
-require("scrollEOF").setup({ insert_mode = true })
