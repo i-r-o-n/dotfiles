5d27df22f4c289af1eb55ada20f564d0e0a920f4
dre <a@alipson.me>
2025-04-22 00:00:20 -0700

    comment out old ultisnips completions config

--- nvim/lua/plugins/lsp/snippets.lua
+++ nvim/lua/plugins/lsp/snippets.lua
@@ -27,7 +27,7 @@ return {
       --
       --
       ls.config.setup({
-        enable_autosnippets = true,
+        -- enable_autosnippets = true,
         region_check_events = "InsertEnter",
         delete_check_events = "InsertLeave",
       })
@@ -50,71 +50,71 @@ return {
   -- { "micangl/cmp-vimtex" },
   -- TODO: disable Luasnip in tex tiletype buffers
   -- also, maybe remove this?
-  {
-    "hrsh7th/nvim-cmp",
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      -- opts.sources = opts.sources or {}
-      -- table.insert(opts.sources, { name = "crates" })
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local cmp = require("cmp")
-      local ultisnips = require("cmp_nvim_ultisnips.mappings")
-
-      -- check if LuaSnip should be used based on buffer file type
-      local use_luasnip = vim.bo.filetype ~= "tex"
-      local luasnip = use_luasnip and require("luasnip") or nil
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            if luasnip then
-              luasnip.lsp_expand(args.body)
-            end
-            vim.fn["UltiSnips#Anon"](args.body)
-          end,
-        },
-      })
-
-      local function ultisnip_can_expand_or_jump()
-        return vim.api.nvim_call_function("UltiSnips#CanExpandSnippet", {}) == 1
-          or vim.api.nvim_call_function("UltiSnips#CanJumpForwards", {}) == 1
-      end
-
-      local function ultisnip_can_jump_back()
-        return vim.api.nvim_call_function("UltiSnips#CanJumpBackwards", {}) == 1
-      end
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif ultisnip_can_expand_or_jump() then
-            ultisnips.expand_or_jump_forwards(fallback)
-          elseif luasnip and luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif ultisnip_can_jump_back() then
-            ultisnips.jump_backwards(fallback)
-          elseif luasnip and luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
-  },
+  -- {
+  --   "hrsh7th/nvim-cmp",
+  --   ---@param opts cmp.ConfigSchema
+  --   opts = function(_, opts)
+  --     -- opts.sources = opts.sources or {}
+  --     -- table.insert(opts.sources, { name = "crates" })
+  --     local has_words_before = function()
+  --       unpack = unpack or table.unpack
+  --       local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  --       return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  --     end
+  --
+  --     local cmp = require("cmp")
+  --     local ultisnips = require("cmp_nvim_ultisnips.mappings")
+  --
+  --     -- check if LuaSnip should be used based on buffer file type
+  --     local use_luasnip = vim.bo.filetype ~= "tex"
+  --     local luasnip = use_luasnip and require("luasnip") or nil
+  --
+  --     cmp.setup({
+  --       snippet = {
+  --         expand = function(args)
+  --           if luasnip then
+  --             luasnip.lsp_expand(args.body)
+  --           end
+  --           vim.fn["UltiSnips#Anon"](args.body)
+  --         end,
+  --       },
+  --     })
+  --
+  --     local function ultisnip_can_expand_or_jump()
+  --       return vim.api.nvim_call_function("UltiSnips#CanExpandSnippet", {}) == 1
+  --         or vim.api.nvim_call_function("UltiSnips#CanJumpForwards", {}) == 1
+  --     end
+  --
+  --     local function ultisnip_can_jump_back()
+  --       return vim.api.nvim_call_function("UltiSnips#CanJumpBackwards", {}) == 1
+  --     end
+  --
+  --     opts.mapping = vim.tbl_extend("force", opts.mapping, {
+  --       ["<Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_next_item()
+  --         elseif ultisnip_can_expand_or_jump() then
+  --           ultisnips.expand_or_jump_forwards(fallback)
+  --         elseif luasnip and luasnip.expand_or_jumpable() then
+  --           luasnip.expand_or_jump()
+  --         elseif has_words_before() then
+  --           cmp.complete()
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --       ["<S-Tab>"] = cmp.mapping(function(fallback)
+  --         if cmp.visible() then
+  --           cmp.select_prev_item()
+  --         elseif ultisnip_can_jump_back() then
+  --           ultisnips.jump_backwards(fallback)
+  --         elseif luasnip and luasnip.jumpable(-1) then
+  --           luasnip.jump(-1)
+  --         else
+  --           fallback()
+  --         end
+  --       end, { "i", "s" }),
+  --     })
+  --   end,
+  -- },
 }
