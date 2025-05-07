d08d82e3dd76f64c7c7ab8429ec564e8bf395f5a
dre <a@alipson.me>
2025-05-06 19:33:15 -0700

    reopen dashboard when all buffers are closed

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -32,6 +32,22 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
 })
 
+-- reopen dashboard on all buffers closed
+vim.api.nvim_create_augroup("DashboardAutoOpen", { clear = true })
+vim.api.nvim_create_autocmd({ "BufDelete" }, {
+  group = "DashboardAutoOpen",
+  callback = function()
+    vim.schedule(function()
+      local buffers = vim.tbl_filter(function(buf)
+        return vim.api.nvim_buf_is_valid(buf) and vim.bo[buf].buflisted and vim.api.nvim_buf_get_name(buf) ~= ""
+      end, vim.api.nvim_list_bufs())
+      if #buffers == 0 then
+        Snacks.dashboard({ win = 0, buf = 0 })
+      end
+    end)
+  end,
+})
+
 -- force set file type
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   pattern = "*",
