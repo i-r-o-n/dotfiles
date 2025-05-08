33003050b80de68ac841e5642c29dd0b7ffda952
dre <a@alipson.me>
2025-05-07 22:33:52 -0700

    update yazi nvim plugin config

--- nvim/lua/plugins/ui/yazi.lua
+++ nvim/lua/plugins/ui/yazi.lua
@@ -3,17 +3,13 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     enabled = false,
   },
-  ---@type LazySpec
   {
     "mikavilpas/yazi.nvim",
     event = "VeryLazy",
     dependencies = {
-      -- check the installation instructions at
-      -- https://github.com/folke/snacks.nvim
       "folke/snacks.nvim",
     },
     keys = {
-      -- 👇 in this section, choose your own keymappings!
       {
         "<leader>e",
         mode = { "n", "v" },
@@ -21,7 +17,6 @@ return {
         desc = "Open file manager at current file",
       },
       {
-        -- Open in the current working directory
         "<leader>E",
         "<cmd>Yazi cwd<cr>",
         desc = "Open file manager in working directory",
@@ -32,15 +27,12 @@ return {
         desc = "Resume previous file manager session",
       },
     },
-    ---@type YaziConfig | {}
     opts = {
-      -- if you want to open yazi instead of netrw, see below for more info
-      open_for_directories = false,
+      open_for_directories = true,
       keymaps = {
         show_help = "<f1>",
       },
     },
-    -- 👇 if you use `open_for_directories=true`, this is recommended
     init = function()
       -- More details: https://github.com/mikavilpas/yazi.nvim/issues/802
       -- vim.g.loaded_netrw = 1
