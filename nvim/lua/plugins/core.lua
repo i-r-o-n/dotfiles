c122a30f6481c4132987b3feed6328200f3f82ba
dre <a@alipson.me>
2025-05-12 22:51:47 -0700

    remove mason-v2 fix, latest update pins v1
    this will be sufficient as i don't particularly need new features right
    now

--- nvim/lua/plugins/core.lua
+++ nvim/lua/plugins/core.lua
@@ -1,7 +1,7 @@
 return {
-  {
-    "LazyVim/LazyVim",
-    url = "https://github.com/dpetka2001/LazyVim",
-    branch = "fix/mason-v2",
-  },
+  -- {
+  --   "LazyVim/LazyVim",
+  --   url = "https://github.com/dpetka2001/LazyVim",
+  --   branch = "fix/mason-v2",
+  -- },
 }
